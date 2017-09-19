e0c9182c08bb2c3922d777363182d710f318b528
nomoory <thsty91@gmail.com>
2017-09-19 11:46:34 +0900

    set customizing interface of babel and webpack

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// This file is not going through babel transformation.
+// So, we write it in vanilla JS
+// (But you could use ES2015 features supported by your Node.js version)
+
+module.exports = {
+  webpack: (webpackConfig, { dev }) => {
+    // Perform customizations to config
+    // Important: return the modified config
+    return webpackConfig
+    }
+}
