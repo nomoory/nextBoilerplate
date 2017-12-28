deb1a3597479fb190dcda999d1c5dd2045676d5d
nomoory <thsty91@gmail.com>
2017-12-28 19:11:45 +0900

    put border line on template

--- src/components/TemplateComponent/style.js
+++ src/components/TemplateComponent/style.js
@@ -1,12 +1,10 @@
+let border = '#dddddd solid 0.5px';
 let staticStyle = {
     mainStyle: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%',
+        position: 'relative',
         display: 'flex',
         flexDirection: 'row',
+        border,
     },
 }
 
